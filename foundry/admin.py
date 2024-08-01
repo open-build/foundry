efc3d3e6973f0f34bffd92157774c8593b36e5cc
Greg Lind <gwlind@gmail.com>
2024-08-01 07:22:02 -0700

    admin

--- foundry/admin.py
+++ foundry/admin.py
@@ -1,14 +1,17 @@
 from .models import *
 from django.contrib import admin
-
-admin.site.register(Company, CompanyAdmin)
-admin.site.register(Foundry, FoundryAdmin)
-admin.site.register(StartupApplication, StartupApplicationAdmin)
-admin.site.register(EvaluationScores, EvaluationScoresAdmin)
-
+from import_export.admin import ImportExportModelAdmin
 from import_export import resources
 
 class StartupApplicationResource(resources.ModelResource):
 
     class Meta:
         model = StartupApplication  # or 'core.Book'
+
+class StartupApplicationAdmin(ImportExportModelAdmin):
+    resource_classes = [StartupApplicationResource]
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Foundry, FoundryAdmin)
+admin.site.register(StartupApplication, StartupApplicationAdmin)
+admin.site.register(EvaluationScores, EvaluationScoresAdmin)
