cc9ff271386b2bd6288dde2fd502242bf78ce55d
Greg Lind <gwlind@gmail.com>
2024-08-01 07:14:27 -0700

    application export

--- foundry/admin.py
+++ foundry/admin.py
@@ -5,3 +5,10 @@ admin.site.register(Company, CompanyAdmin)
 admin.site.register(Foundry, FoundryAdmin)
 admin.site.register(StartupApplication, StartupApplicationAdmin)
 admin.site.register(EvaluationScores, EvaluationScoresAdmin)
+
+from import_export import resources
+
+class StartupApplicationResource(resources.ModelResource):
+
+    class Meta:
+        model = StartupApplication  # or 'core.Book'
